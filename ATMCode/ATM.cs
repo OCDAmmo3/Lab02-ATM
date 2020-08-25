c2de841294db1f5fad5b6a5a68a491a664d48275
OCDAmmo3 <44117578+OCDAmmo3@users.noreply.github.com>
2020-08-25 16:04:54 -0500

    Made working method to view balance and broken withdrawal

--- ATMCode/ATM.cs
+++ ATMCode/ATM.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace ATMCode
 {
-    class ATM
+    public class ATM
     {
+        static public decimal Balance = 10.00m;
+        public static void Main(string[] args)
+        {
+            try
+            {
+                UserInterface();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void UserInterface()
+        {
+            Console.WriteLine("Do things");
+        }
+
+        public static decimal ViewBalance()
+        {
+            return Balance;
+        }
+
+        public static decimal Withdraw(decimal number)
+        {
+            return number;
+        }
     }
 }
--- ATMTests.Tests/ATMTests.cs
+++ ATMTests.Tests/ATMTests.cs
@@ -1,10 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ATMCode;
+using Xunit;
 
 namespace ATMTests.Tests
 {
-    class ATMTests
+    public class ATMTests
     {
+        [Fact]
+        public static void ViewBalance_should_return_balance()
+        {
+            // No arrange, as the balance will come from the ATM file "ViewBalance" method.
+
+            // Act
+            decimal result = ATM.ViewBalance();
+
+            // Assert
+            Assert.Equal(10.00m, result);
+        }
+
+        [Fact]
+        public static void Withdraw_should_return_requested_amount()
+        {
+            // Arrange
+            decimal withdrawal = 7.50m;
+
+            // Act
+            decimal result = ATM.Withdraw(withdrawal);
+            decimal currentBalance = ATM.ViewBalance();
+
+            // Assert
+            Assert.Equal(7.50m, result);
+            Assert.Equal(2.50m, currentBalance);
+        }
     }
 }
